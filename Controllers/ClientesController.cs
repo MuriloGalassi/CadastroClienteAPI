3d51be39702104cc3ea5ba9496cc2048e4cfade1
Murilo Pedro Galassi <murilo.galassi@educacao.sp.gov.br>
2024-10-29 16:48:28 -0300

    Primeira atualização

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using CadastroClienteAPI.Data;
+using CadastroClienteAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+
+namespace CadastroClienteAPI.Controllers
+{
+    
+}
